2923a0c7ed79100184bb54e1afd862e9884ed685
isaac545454 <isaac.newton1221@gmail.com>
2022-12-31 08:23:10 -0300

    feat: create user backend

--- back/controllers/userController.js
+++ back/controllers/userController.js
@@ -12,7 +12,43 @@ const generaToken = (id)=>{
 }
 
 const register = async (req, res)=>{
-  res.send("registro")
+  const {email, name, password} = req.body
+
+  //garantindo que o usuario não existe 
+  const user = await User.findOne({email})
+   
+  if(user){
+    res.status(422).json({errors: ["por favor, utilize outro email"]})
+    return 
+   }
+
+   //gerando hash 
+   const salt = await bcrypt.genSalt()
+   const passwordHash = await bcrypt.hash(password, salt)
+  
+
+   //criando usuario 
+   const newUser = await User.create({
+    name,
+    email,
+    password: passwordHash,
+  })
+
+  //usuario criado 
+  if(!newUser){
+    res.status(422).json({errors: ["Não foi possível criar usuário."]})
+    return 
+    
+   }
+
+   res.status(201).json({
+    _id: newUser._id, 
+    token: generaToken(newUser._id)
+   })
+
+
+
+
 }
 
 module.exports = {

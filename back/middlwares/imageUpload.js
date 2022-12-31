23096bcd1596c7196016bd1d18fe2a33e069ad75
isaac545454 <isaac.newton1221@gmail.com>
2022-12-31 17:14:19 -0300

    feat: config upload

---
+++ back/middlwares/imageUpload.js
@@ -0,0 +1,31 @@
+const multer = require('multer');
+const path = require('path');
+
+
+//destination 
+const imageStorage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        let folter = ""
+        if(req.baseUrl.includes("users")){
+            folder = "users"
+        }  else if(req.baseUrl.includes("photos")){
+          folder = "photos"
+        }
+        cb(null, `uploads/${folder}/`) 
+    },
+    filename:(req, file, cb)=>{
+        cb(null, Date.now() +  path.extname(file.originalname));
+    }
+})
+
+const imageUpload = multer({
+    storage: imageStorage,
+    fileFilter: function(req, file, cb){
+        if(!file.originalname.match(/\.(png|jpg)$/)){
+            return cb(new Error("por favor envie apenas pnj ou jpg"))
+        }
+        cb(undefined, true)
+    },
+})
+
+module.exports = { imageUpload}

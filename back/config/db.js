20b18be8c43cefab8dc00a13f1023bb9a57408a1
isaac545454 <isaac.newton1221@gmail.com>
2022-12-31 08:01:41 -0300

    feat: validation router register user

--- back/config/db.js
+++ back/config/db.js
@@ -1,12 +1,14 @@
 const mongoose = require("mongoose")
 //mongodb+srv://isaac:<password>@cluster0.hjhia.mongodb.net/?retryWrites=true&w=majority
+//mongodb+srv://isaac:<password>@cluster0.hjhia.mongodb.net/?retryWrites=true&w=majority
+
 const dbUser = process.env.USER
 const dbPassword = process.env.PASSWORD
 
 const conn = async()=>{
   try {
     const dbConn = await 
-    mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.hjhia.mongodb.net/?retryWrites=true&w=majority`)
+    mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.p2x8fjn.mongodb.net/?retryWrites=true&w=majority`)
      console.log("conectado ao banco");
      return dbConn
   } catch (error) {
--- back/middlwares/userValidation.js
+++ back/middlwares/userValidation.js
@@ -2,8 +2,32 @@ const {body} = require("express-validator")
 
 const userCreateValidation = ()=>{
   return [body("name")
-  .isString().withMessage("o nome é obrigatorio")
-  .isLength({min: 4}).withMessage("o nome precisa de 4 caracteres no minimo")]
+  .isString()
+  .withMessage("o nome é obrigatorio")
+  .isLength({min: 4})
+  .withMessage("o nome precisa de 4 caracteres no minimo"),
+  body("email") 
+  .isString()
+  .withMessage("o email é obrigatorio") 
+  .isEmail()
+  .withMessage("insira um email valido"),
+  body("password")
+ .isString()
+ .withMessage("o password é obrigatorio")
+ .isLength({min: 6})
+ .withMessage("o password precisa de 6 caracteres no minimo"),
+  body("confirmPassword") 
+  .isString()
+  .withMessage("a confirmação de password é obrigatoria")
+  .custom((value, {req})=>{
+  if(value!== req.body.password){
+      throw new Error("as senhas não são iguais")
+   }
+   return true
+  }
+  )
+
+]
 }
 
 module.exports = {userCreateValidation}

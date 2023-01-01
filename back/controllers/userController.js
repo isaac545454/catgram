0823eb36417413c889f1d4d41b83a6064932edcb
isaac545454 <isaac.newton1221@gmail.com>
2023-01-01 07:35:52 -0300

    feat: update user backend

--- back/controllers/userController.js
+++ back/controllers/userController.js
@@ -1,6 +1,7 @@
 const User = require("../models/User")
 const bcrypt = require('bcryptjs')
 const jwt = require("jsonwebtoken")
+const  mongoose  = require("mongoose")
 const jwtSecret = process.env.SECRET 
 
 //token generico 
@@ -80,7 +81,36 @@ const getCurrentUser = async (req, res) => {
 }
 
 const update = async (req, res) => {
-res.send("update")
+ const {name, password, bio} = req.body 
+ let profileImage = null 
+ if(req.file){
+  profileImage = req.file.filename
+ }
+ const reqUser = req.user
+ const user = await User.findById(mongoose.Types.ObjectId(reqUser._id)).select("-password")
+ 
+ if(name){
+  user.name = name
+
+ }
+ if(password){
+  const salt = await bcrypt.genSalt()
+  const passwordHash = await bcrypt.hash(password, salt)
+  user.password = passwordHash
+ }
+ if(profileImage){
+  user.profileImage = profileImage
+
+ }
+ if(bio){
+  user.bio = bio
+ }
+
+ await user.save()
+
+ res.status(200).json(user)
+
+
 }
 
 module.exports = {

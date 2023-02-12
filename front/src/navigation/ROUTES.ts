4c7770a4e894667df90af7a92c0040eaca5754e7
isaac545454 <isaac.newton1221@gmail.com>
2023-02-12 10:41:49 -0300

    refactor: routes

--- front/src/navigation/ROUTES.ts
+++ front/src/navigation/ROUTES.ts
@@ -1,6 +1,6 @@
 export const ROUTES = {
   home: "/",
-  login: "/login",
+  login: "/",
   register: "/register",
   user: (id: string) => `/users/${id}`,
   profile: "/profile",
--- front/src/navigation/index.tsx
+++ front/src/navigation/index.tsx
@@ -1,21 +1,10 @@
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../context/index";
-
-//
-import Home from "../pages/Home/Home";
-import Login from "../pages/Auth/Login/index";
-import Register from "../pages/Auth/Register/index";
-import EditProfile from "../pages/EditPhofile";
-import Phofire from "../pages/Profile";
-import PhotosView from "../pages/photoView";
-import Search from "../pages/Search";
-
+import { AuthRoutes, RoutesFinish } from "./auth";
 //
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-
-import { ROUTES } from "./ROUTES";
 
 function RoutesDestination() {
   const { auth, isLoading } = useContext(AuthContext);
@@ -33,70 +22,7 @@ function RoutesDestination() {
       <BrowserRouter>
         <Navbar />
         <div className="min-h-[70vh]">
-          <Routes>
-            <Route
-              path={ROUTES.home}
-              element={
-                !auth && !isLoading ? <Navigate to={ROUTES.login} /> : <Home />
-              }
-            />
-            <Route
-              path={ROUTES.login}
-              element={
-                !auth && !isLoading ? <Login /> : <Navigate to={ROUTES.login} />
-              }
-            />
-            <Route
-              path={ROUTES.register}
-              element={
-                !auth && !isLoading ? (
-                  <Register />
-                ) : (
-                  <Navigate to={ROUTES.home} />
-                )
-              }
-            />
-            <Route
-              path={ROUTES.profile}
-              element={
-                !auth && !isLoading ? (
-                  <Navigate to={ROUTES.login} />
-                ) : (
-                  <EditProfile />
-                )
-              }
-            />
-            <Route
-              path={ROUTES.user(":id")}
-              element={
-                !auth && !isLoading ? (
-                  <Navigate to={ROUTES.login} />
-                ) : (
-                  <Phofire />
-                )
-              }
-            />
-            <Route
-              path={ROUTES.photosView(":id")}
-              element={
-                !auth && !isLoading ? (
-                  <Navigate to={ROUTES.login} />
-                ) : (
-                  <PhotosView />
-                )
-              }
-            />
-            <Route
-              path={ROUTES.search(":text")}
-              element={
-                !auth && !isLoading ? (
-                  <Navigate to={ROUTES.login} />
-                ) : (
-                  <Search />
-                )
-              }
-            />
-          </Routes>
+          {auth ? <RoutesFinish /> : <AuthRoutes />}
         </div>
         <Footer />
       </BrowserRouter>
